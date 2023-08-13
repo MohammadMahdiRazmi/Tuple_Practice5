cc8a61762b629e58d14aa646a057a6ec4301f522
MahdiRazmi <109846619+MohammadMahdiRazmi@users.noreply.github.com>
2023-08-13 10:44:47 +0330

    Practice5

---
+++ Practice.py
@@ -0,0 +1,17 @@
+while True:
+    user_tuple = (10, 20, 30, 40, 50)  # تاپل موردنظر خود را وارد کنید
+
+    print("Tuple mojod:", user_tuple) 
+
+    index = int(input("lotfan yek index vared konid:"))
+    len_tuple = len(user_tuple)
+    if 0 <= index < len(user_tuple):
+        selected_item = user_tuple[index]
+        print(f"item entekhab shode {selected_item}")
+        print(f"index item entekhab shode {index}")
+    else:
+        print(f"index mojod nist! {len_tuple} <--- tedad index ha  ")
+    print("-------------------------------------------------------------")
+
+
+
